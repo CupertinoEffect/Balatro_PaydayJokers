ccaa3b23497d375515d1fca5fe2dc5e2456f5733
CupertinoEffect <aczomcooper@gmail.com>
2025-03-14 00:33:18 +1100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Balatro_PaydayJokers
+# Payday Jokers
+Retextures the Burglar and Midas Mask jokers to resemble Dallas from Payday 2.
+Requires [Malverk](https://balatromods.miraheze.org/wiki/Malverk).
---
+++ main.lua
@@ -0,0 +1,28 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Payday Jokers
+--- MOD_ID: payday
+--- PREFIX: payday
+--- MOD_AUTHOR: [CupertinoEffect]
+--- MOD_DESCRIPTION: Replaces the Burglar and Midas Mask Jokers with Dallas of the Payday Gang.
+--- DEPENDENCIES: [malverk]
+
+AltTexture{
+    key = "payday",
+    set = "Joker",
+    path = "paydalatro.png",
+    keys = {
+        "j_burglar",
+        "j_midas_mask"
+    },
+}
+
+TexturePack{
+	key = "payday",
+	textures = {
+		"payday_payday"
+	},
+	loc_txt = {
+		name = "Payday Jokers",
+		text = { "Replaces the Burglar and", "Midas Mask jokers with", "Dallas of the Payday Gang." }
+	}
+}
